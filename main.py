6422c3d4f27e3eb2e8e1c1825b192442eb7ee1e7
marcin <s12497@pjwstk.edu.pl>
2023-11-06 23:16:44 +0100

    Data_loader, euclidean distance, cosine distance, user finder

--- main.py
+++ main.py
@@ -43,24 +43,39 @@ def find_users(target_user, data, distance_func):
 
 
 # Recommend movies based on similar users' ratings
+# Function to recommend movies based on similar users' ratings
 def recommend_movies(target_user, data, num_recommendations=5):
     # Find users similar to the target user
-    similar_users = find_users(target_user, data, cosine_distance)  # or euclidean_distance
+    similar_users = find_users(target_user, data, cosine_distance)[:5]
+    dissimilar_users = find_users(target_user, data, cosine_distance)[-5:]
 
-    # Dictionary to hold potential recommendations
+    # Dictionaries to hold potential recommendations and non-recommendations
     recommended_movies = {}
+    not_recommended_movies = {}
 
-    # Iterate over similar users and their ratings
+    # Check movies rated by similar users
     for similar_user, _ in similar_users:
         for movie, rating in data[similar_user].items():
-            # If the target user hasn't rated this movie and the similar user rated it highly
-            if movie not in data[target_user] and rating >= 8:  # assuming 8 is the threshold for a high rating
-                # Add to the recommended_movies or update the score
-                recommended_movies[movie] = recommended_movies.get(movie, 0) + rating
+            if movie not in data[target_user]:  # Target user hasn't seen/rated it
+                if rating >= 8:  # Assuming 8 is the threshold for a high rating
+                    recommended_movies[movie] = recommended_movies.get(movie, 0) + rating
+                else:  # Low-rated movies by similar users
+                    not_recommended_movies[movie] = not_recommended_movies.get(movie, 0) + rating
 
-    # Sort the movies based on the aggregated score and return the top ones
+    # Check movies rated by dissimilar users
+    for dissimilar_user, _ in dissimilar_users:
+        for movie, rating in data[dissimilar_user].items():
+            if movie not in data[target_user]:  # Target user hasn't seen/rated it
+                if rating <= 5:  # Assuming 5 is the threshold for a low rating
+                    not_recommended_movies[movie] = not_recommended_movies.get(movie, 0) + rating
+
+    # Sort the movies based on the aggregated score for recommendations
     sorted_recommendations = sorted(recommended_movies.items(), key=lambda x: x[1], reverse=True)
-    return sorted_recommendations[:num_recommendations]
+    # Sort the movies based on the aggregated score for non-recommendations
+    sorted_non_recommendations = sorted(not_recommended_movies.items(), key=lambda x: x[1], reverse=False)
+
+    # Return the top recommended and top 5 non-recommended movies
+    return sorted_recommendations[:num_recommendations], sorted_non_recommendations[:5]
 
 
 def display_similarities(target_user, data):
@@ -91,14 +106,18 @@ def display_similarities(target_user, data):
 
 
 def main(json_data, target_user):
-
-
     print(f"Finding similar and dissimilar users for {target_user}...\n")
     display_similarities(target_user, json_data)
 
-    print(f"\nRecommended movies for {target_user} based on similar user preferences:")
-    recommended = recommend_movies(target_user, json_data)
+    recommended, not_recommended = recommend_movies(target_user, json_data)
+    print("-" * 40)
+    print("Recommended movies based on similar user preferences:")
     for movie, score in recommended:
+        print(f"{movie}: {score}")
+
+    print("-" * 40)
+    print("\nMovies not recommended based on user preferences:")
+    for movie, score in not_recommended:
         print(f"{movie}: {score}")
 
 
